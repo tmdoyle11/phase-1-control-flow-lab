148413cb30b628b12f065ce857da080489b32506
Thomas Doyle <tmdoyle11@gmail.com>
2022-03-07 14:39:31 -0600

    commit tree f83ce2e24d306acb9bd2f2be9259ce516d89f287

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400) {
+    return 'This one is on me!';
+  }
+  else if (feet > 2000 && feet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (feet > 2500) {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  const cityCheck = (city === 'NYC') ? ('Ok, sounds good.') : ('No go.');
+  
+  return cityCheck;
 }
 
 function switchOnCharmFromTip(){
